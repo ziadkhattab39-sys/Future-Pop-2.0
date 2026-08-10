9b0ac3bfb0b5ab7bede03a7ffc964483ce766222
ziadkhattab39-sys <ziadkhattab39@gmail.com>
2026-08-10 04:55:16 +0300

    Completed Parent Class

--- Create.py
+++ Create.py
@@ -1,6 +1,5 @@
 import pandas as pd
 Data = pd.read_excel("Unified_Form_Formatted.xlsx")
-print(Data.columns)
 from Governorate import Governorate
 Govs = []
 
@@ -16,9 +15,25 @@ for index, row in Data.iterrows() :
     area = row["Area"]
     Gov = Governorate(name, capital, type, pop2018, pop2020, pop2022, pop2024, pop2026, area)
     Govs.append(Gov)
+
 Govs[0].Calc_Pop_Growth_Rate()
 Govs[0].Calc_Pop_Density()
 Govs[0].Calc_Growth_Rate_Drop()
+Govs[0].Calc_Pred_Growth_Rate()
+Govs[0].Calc_Pred_Pop_Count()
 print(Govs[0].Pop_Growth_Rate)
 print(Govs[0].Pop_Density)
 print(Govs[0].Growth_Rate_Drop)
+print(Govs[0].Pred_Growth_Rate)
+print(Govs[0].Pred_Pop_Count)
+print()
+Govs[1].Calc_Pop_Growth_Rate()
+Govs[1].Calc_Pop_Density()
+Govs[1].Calc_Growth_Rate_Drop()
+Govs[1].Calc_Pred_Growth_Rate()
+Govs[1].Calc_Pred_Pop_Count()
+print(Govs[1].Pop_Growth_Rate)
+print(Govs[1].Pop_Density)
+print(Govs[1].Growth_Rate_Drop)
+print(Govs[1].Pred_Growth_Rate)
+print(Govs[1].Pred_Pop_Count)
